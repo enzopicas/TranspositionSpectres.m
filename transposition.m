d0fa4504f41858105e4806fa0a0c9139d1dd8bc8
enzopicas <picasenzo@gmail.com>
2019-03-26 17:38:25 +0100

    Ajout du calcul de changement d'échelle

--- transposition.m
+++ transposition.m
@@ -1,5 +1,22 @@
-function transposition(spectre_txt)
-    spectre = load(spectre_txt);
+function spectre_Hz = transposition(spectre_txt)
+close all;
+
+    spectre_nm = load(spectre_txt); %Chargement du spectre
     
-    tracerSpectre(spectre); %Tracé du spectre entré dans la fonction
+    tracerSpectre(spectre_nm); %Tracé du spectre entré dans la fonction
+    
+    %--------------------------
+    % TRANSPOSITION
+    %--------------------------
+    Fmin = 280; Fmax = 8000;
+    Lmin = 300; Lmax = 800;
+    
+    %Calcul de la nouvelle échelle
+    a=(Fmax-Fmin)/(Lmax-Lmin); %a=(log10(Fmax)-log10(Fmin))/(Lmax-Lmin);
+    b=Fmin;
+    spectre_Hz = spectre_nm * a + b;
+    
+    %Fabrication du signal audio
+    
+    
 end

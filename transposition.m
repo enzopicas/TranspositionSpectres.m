7fd47e5a7a7c4d1daed3cd101a61ecb7fbfc4334
enzopicas <picasenzo@gmail.com>
2019-03-28 18:05:43 +0100

    Ajout de la documentation

--- transposition.m
+++ transposition.m
@@ -1,3 +1,15 @@
+%Cette fonction permet de tracer, et de transposer un spectre de raies en 
+%un signal acoustique.
+%---------Utilisation-----------
+%Le spectre de raies doit être entré en paramètre de la
+%fonction au travers d'un fichier texte. 
+%Les différentes longueurs d'onde doivent être entrées ligne par ligne, 
+%avec une décimale différente de 0.
+%La fonction renvoie une figure représentant les différentes raies, et joue
+%le son associé au spectre.
+%---------Fonction liée---------
+% tracerSpectre.m
+
 function [spectre_nm, spectre_Hz] = transposition(spectre_txt)
 close all;
 
@@ -11,7 +23,6 @@ close all;
     Fmin = 500; Fmax = 6000;
     Lmin = 300; Lmax = 800;
     
-    %Calcul de la nouvelle échelle
     a=(Fmax-Fmin)/(Lmax-Lmin);
     b=Fmax-a*Lmax;
     spectre_Hz = spectre_nm * a + b;

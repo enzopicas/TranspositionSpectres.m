2a2d68d13facf0d80bc3d24edc53af284d572ca8
enzopicas <picasenzo@gmail.com>
2019-03-30 18:22:18 +0100

    Prise en compte de l'amplitude dans le tracé du spectre

--- tracerSpectre.m
+++ tracerSpectre.m
@@ -3,26 +3,29 @@ function tracerSpectre(spectre)
     Lmax=800; Lmin=300;
 
     REF = zeros((Lmax-Lmin),1); %Une case par longueur d'onde
-    valeursREF = Lmin:Lmax-1; %Uniquement pour tracer à la bonne échelle
+    valeursREF = Lmin:Lmax-1; %Uniquement pour tracer à la bonne échelle x
     
     for i = 1:length(spectre)
         for n = 1:length(REF)
-           if ((spectre(i) > n+Lmin) && (spectre(i) < n+(Lmin+1)))
-               REF(n) = REF(n) + 1;
-               REF(n+1) = REF(n+1) +1;
+           if ((spectre(i,1) > n+Lmin) && (spectre(i,1) < n+(Lmin+1)))
+               REF(n) = spectre(i,2);
            end
         end
     end
+    
+    %Tracé de la figure
     A = round(length(valeursREF)/3);
     B = round(2*length(valeursREF)/3);
-    % Tracé de la figure
+    
     figure;
     hold on
     plot(valeursREF(1:A), REF(1:A),'b');
     plot(valeursREF(A:B), REF(A:B),'g');
     plot(valeursREF(B:end), REF(B:end),'r');
     hold off
-    ylim([0.95 , 1]); set(gca,'ytick',[]); xlim([Lmin-10 , Lmax+10]);
+    
+    xlim([Lmin-10 , Lmax+10]);
     xlabel('Longueur d''onde en nm'); 
+    ylabel('Amplitude des raies');
     title('Spectre de raies');
 end

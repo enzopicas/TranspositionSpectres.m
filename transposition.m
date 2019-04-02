56caf65de148728b882547dc1c95870fe2740bdd
enzopicas <picasenzo@gmail.com>
2019-04-02 18:48:44 +0200

    Modification de la frequence d'échantillonage à 44100 (standard), audioplayer sorti de la boucle (transposition toujours linéaire)

--- transposition.m
+++ transposition.m
@@ -4,7 +4,8 @@
 %Le spectre de raies doit être entré en paramètre de la
 %fonction au travers d'un fichier texte. 
 %Les différentes longueurs d'onde doivent être entrées ligne par ligne, 
-%avec une décimale différente de 0.
+%avec une décimale différente de 0, les amplitudes associées à ces
+%longueurs d'onde doivent êtres inscrites après un espace.
 %La fonction renvoie une figure représentant les différentes raies, et joue
 %le son associé au spectre.
 %---------Fonction liée---------
@@ -23,19 +24,24 @@ close all;
     Fmin = 500; Fmax = 6000;
     Lmin = 300; Lmax = 800;
     
+    %--> Transposition en linéaire
     a=(Fmax-Fmin)/(Lmax-Lmin);
     b=Fmax-a*Lmax;
-    spectre_Hz = spectre_nm * a + b;
+    spectre_Hz = spectre_nm(:,1) * a + b;
     
     %--------------------------
     % SIGNAL AUDIO
     %--------------------------
-    fe = 8000; N = 8192; t = (1:N)/fe;
+    fe = 44100; T = 1;
+    t = 0:1/fe:T;
+    
+    audio = [];
     for i = 1:length(spectre_Hz)
-        audio = sin(2*pi*spectre_Hz(i)*t);
-        player = audioplayer(audio, fe);
-        playblocking(player)
-        clear player
+        audio = [audio sin(2*pi*spectre_Hz(i)*t)];
     end
+    
+    player = audioplayer(audio, fe);
+    playblocking(player)
+    clear player
     
 end
